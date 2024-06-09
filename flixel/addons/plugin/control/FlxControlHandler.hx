598fe2b84238d18e40e3243824aef554846b35b8
swordcube <swordsoftarchive@gmail.com>
2024-06-09 17:18:47 -0500

    Change all tick-based values from Ints to Floats
    Reasoning is to allow for more accurate elapsed values of more than 3 decimal points

--- flixel/addons/plugin/control/FlxControlHandler.hx
+++ flixel/addons/plugin/control/FlxControlHandler.hx
@@ -150,9 +150,9 @@ class FlxControlHandler
 	// The ms delay between firing when the key is held down
 	var _fireRate:Int;
 	// The internal time when they can next fire
-	var _nextFireTime:Int = 0;
+	var _nextFireTime:Float = 0;
 	// The internal time of when when they last fired
-	var _lastFiredTime:Int;
+	var _lastFiredTime:Float;
 	// The fire key mode
 	var _fireKeyMode:Int;
 	// A function to call every time they fire
@@ -165,13 +165,13 @@ class FlxControlHandler
 	// The jump key mode
 	var _jumpKeyMode:Int;
 	// The internal time when they can next jump
-	var _nextJumpTime:Int;
+	var _nextJumpTime:Float;
 	// The internal time of when when they last jumped
-	var _lastJumpTime:Int;
+	var _lastJumpTime:Float;
 	// A short window of opportunity for them to jump having just fallen off the edge of a surface
-	var _jumpFromFallTime:Int;
+	var _jumpFromFallTime:Float;
 	// Internal time of when they last collided with a valid jumpSurface
-	var _extraSurfaceTime:Int;
+	var _extraSurfaceTime:Float;
 	// The surfaces they can jump from (i.e. FLOOR)
 	var _jumpSurface:Int;
 	// A function to call every time they jump
@@ -920,7 +920,7 @@ class FlxControlHandler
 					_lastFiredTime = FlxG.game.ticks;
 					_fireCallback();
 					fired = true;
-					_nextFireTime = _lastFiredTime + Std.int(_fireRate / FlxG.timeScale);
+					_nextFireTime = _lastFiredTime + (_fireRate / FlxG.timeScale);
 				}
 			}
 			else
@@ -1004,7 +1004,7 @@ class FlxControlHandler
 			}
 
 			_lastJumpTime = FlxG.game.ticks;
-			_nextJumpTime = _lastJumpTime + Std.int(_jumpRate / FlxG.timeScale);
+			_nextJumpTime = _lastJumpTime + (_jumpRate / FlxG.timeScale);
 
 			jumped = true;
 		}
